d00f7a74fca2cbfbd0f144c06b1812f4e0c453fd
Supratik <supratik@fusioncharts.com>
2019-03-09 22:12:34 +0530

    Added dropdown constant values

---
+++ src/constants/drop-down-list.js
@@ -0,0 +1,43 @@
+export const location = [{
+  text: 'Koramangala',
+  value: 'Koramangala',
+  key: 'Koramangala'
+}, {
+  text: 'HSR Layout',
+  value: 'HSR Layout',
+  key: 'HSR Layout'
+}, {
+  text: 'Indiranagar',
+  value: 'Indiranagar',
+  key: 'Indiranagar'
+}];
+
+export const availability = [{
+  text: 'Monday',
+  value: 'Mon',
+  key: 'Mon'
+}, {
+  text: 'Tuesday',
+  value: 'Tue',
+  key: 'Tue'
+}, {
+  text: 'Wednesday',
+  value: 'Wed',
+  key: 'Wed'
+}, {
+  text: 'Thursday',
+  value: 'Thu',
+  key: 'Thu'
+}, {
+  text: 'Friday',
+  value: 'Fri',
+  key: 'Fri'
+}, {
+  text: 'Saturday',
+  value: 'Sat',
+  key: 'Sat'
+}, {
+  text: 'Sunday',
+  value: 'Sun',
+  key: 'Sun'
+}];
