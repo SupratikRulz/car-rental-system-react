2fb7d21224f235efd76d5cb9f08f6ae00d26edb9
Supratik <supratik@fusioncharts.com>
2019-03-09 22:14:33 +0530

    Implemented landing page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Dropdown, Button } from 'semantic-ui-react';
+import {location, availability} from './../constants/drop-down-list';
+
+import './css/LandingPage.css';
+
+let locationValues = [],
+  dayValues = [];
+
+export default function LandingPage(props) {
+  return (
+    <div className='LandingPage'>
+      <Dropdown
+        placeholder='Select Location'
+        fluid
+        multiple
+        search
+        selection
+        options={location}
+        className='dropdown-location'
+        onChange={setLocationValues}
+      />
+
+      <Dropdown
+        placeholder='Select Day'
+        fluid
+        multiple
+        search
+        selection
+        options={availability}
+        className='dropdown-availability'
+        onChange={setDayValues}
+      />
+
+      <Button primary className='submit-btn' onClick={() => handleSubmit(props)}>Submit</Button>
+    </div>
+  )
+}
+
+
+function setLocationValues(event, data) {
+  locationValues = data.value;
+}
+
+function setDayValues(event, data) {
+  dayValues = data.value;
+}
+
+function handleSubmit(props) {
+  if (locationValues.length && dayValues.length) {
+    props.updateSearch(locationValues, dayValues);
+  } else {
+    alert('Fields cannot be Empty!')
+  }
+}
